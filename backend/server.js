4f76de6c613a80ebbf713b60280ab7bd3196320d
Douglas Colin Moore <dougcolin@gmail.com>
2023-07-13 20:12:57 -0400

    Confirmed added backend requests for Login, Registration, and Task CRUD operations are fully functional

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-// const colors = require('colors')
-// const dotenv = require('dotenv').config()
+const colors = require('colors')
+const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.port || 5050
