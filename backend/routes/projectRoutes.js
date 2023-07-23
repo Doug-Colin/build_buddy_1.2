040fb33952beabe47e01e947585041ac5a84ade5
Douglas Colin Moore <dougcolin@gmail.com>
2023-07-23 14:35:07 -0400

    feat: Add project routes and integrate into server

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,16 @@
+//note- JS syntax here, syntax will be different for frontend if using Typescript/ts2015
+const express = require('express')
+const router = express.Router()
+const { getProjects, setProject, updateProject, deleteProject } = require('../controllers/projectController')
+
+const { protect } = require('../middleware/authMiddleware')
+
+//protected & combined route for get and post requests (create/read)
+router.route('/').get(protect, getProjects).post(protect, setProject)
+
+//protected & combined route for put and delete requests (update/delete)
+router.route('/:id').delete(protect, deleteProject).put(protect, updateProject)
+
+
+
+module.exports = router
