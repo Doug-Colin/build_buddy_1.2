26c3222b477e10bd8ff688ef303675c88ea19fff
Douglas Colin Moore <dougcolin@gmail.com>
2023-08-02 20:07:56 -0400

    feat(Projects): Add reusable DeletionAlertDialog.tsx component for alerting userupon attempts to delete

---
+++ frontend/src/components/DeletionAlertDialog.tsx
@@ -0,0 +1,50 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+interface DeletionAlertDialogProps {
+  //In parent component, pass a <button> or <Button> with preferred props/styling as value of alertDialogTriggerButton
+  button: React.ReactNode;
+  alertDialogTitle: string;
+  alertDialogDescription: string;
+  alertDialogAction: string;
+  onDelete: () => void;
+}
+
+export default function DeletionAlertDialog({
+  button,
+  alertDialogTitle,
+  alertDialogDescription,
+  alertDialogAction,
+  onDelete,
+}: DeletionAlertDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        {button}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{alertDialogTitle}</AlertDialogTitle>
+          <AlertDialogDescription>
+            {alertDialogDescription}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onDelete}>
+            {alertDialogAction}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
