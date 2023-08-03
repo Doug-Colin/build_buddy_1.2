12d3f8729835436b2107899d6e5c3417d0f59da7
Douglas Colin Moore <dougcolin@gmail.com>
2023-08-02 21:15:52 -0400

    feat(Projects): resolve TS flags/errors in ProjectForm.tsx

--- frontend/src/pages/projects-page/components/ProjectForm.tsx
+++ frontend/src/pages/projects-page/components/ProjectForm.tsx
@@ -7,10 +7,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { DueDatePicker } from "@/pages/projects-page/components/DueDatePicker";
-import { useForm, Controller } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { projectSchema } from "@/validators/projectSchema";
 import {
   Select,
   SelectContent,
@@ -18,10 +14,45 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Button } from "@/components/ui"
+import { DueDatePicker } from "@/pages/projects-page/components/DueDatePicker";
+import { useForm, Controller } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { projectSchema } from "@/validators/projectSchema";
+import { createProject } from "@/features/projects/projectSlice"; 
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import { ProjectFormType } from "@/types/types";
 
+
+/*In parent component, declare state and handler to close FormDialog upon successful form submission:
+
+//state for closing FormDialog upon succesfull form submission:
+const [isFormDialogOpen, setIsFormDialogOpen] = useState(false)
+
+//handler for closing FormDialog child component when form is succesfully submitted:
+  const handleFormDialogClose = (state: boolean) => {
+    setIsFormDialogOpen(state);
+  };
+*/
+
+
+export interface ProjectFormType {
+  projectName: string;
+  client?: string;
+  dueDate: Date;
+  status?: 'In progress' | 'Completed' | "Long-Term";
+}
+interface ProjectFormProps {
+  onFormSubmit: () => void; // Replace with the actual type if different
+}
+
+export default function ProjectForm({onFormSubmit}: ProjectFormProps) {
 type Input = z.infer<typeof projectSchema>;
 
-export default function ProjectForm() {
+
+
+
   const form = useForm<Input>({
     resolver: zodResolver(projectSchema),
     defaultValues: {
@@ -32,9 +63,18 @@ export default function ProjectForm() {
     },
   });
 
-  function onSubmit(data: Input) {
-    alert(JSON.stringify(data, null, 4));
+//check if form is updating state with each keystroke/selection
+console.log(form.watch());
+
+const dispatch = useAppDispatch();
+
+// const { status, error, message } = useAppSelector((state) => state.projects);
+
+
+  function onSubmit(data: ProjectFormType) {
+    dispatch(createProject(data))
     console.log(data);
+    onFormSubmit()
   }
 
   return (
@@ -116,6 +156,7 @@ export default function ProjectForm() {
               </FormItem>
             )}
           />
+          <Button type="submit">Submit</Button>
         </form>
       </Form>
     </div>
