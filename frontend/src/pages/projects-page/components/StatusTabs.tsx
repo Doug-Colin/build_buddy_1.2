53318a3d02e4d208ed3a5e3f692455cd29270697
Douglas Colin Moore <dougcolin@gmail.com>
2023-08-05 03:15:22 -0400

    feat:(Projects): Update StatusTabs.tsx so all tab options only show up when project is being edited

--- frontend/src/pages/projects-page/components/StatusTabs.tsx
+++ frontend/src/pages/projects-page/components/StatusTabs.tsx
@@ -1,19 +1,47 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type StatusTabsProps = {
+  isEditable: boolean;
   value: "In progress" | "Completed" | "Long-Term";
   onChange: (selectedStatus: "In progress" | "Completed" | "Long-Term") => void; //(necessary?)
 };
 
-export default function StatusTabs(props: StatusTabsProps) {
+export default function StatusTabs({
+  isEditable,
+  value,
+  onChange,
+}: StatusTabsProps) {
   return (
-    <Tabs value={props.value} className="w-[350px]">
-      <TabsList className="grid w-full grid-cols-3">
-        <TabsTrigger value="In progress" onClick={() => props.onChange('In progress')}>In progress</TabsTrigger>
-        <TabsTrigger value="Completed" onClick={() => props.onChange('Completed')}>Completed</TabsTrigger>
-        <TabsTrigger value="Long-Term" onClick={() => props.onChange('Long-Term')}>Long-Term</TabsTrigger>
+    <Tabs value={value} className="w-[320px]">
+      <TabsList className="">
+        {isEditable ? (
+          <>
+            <TabsTrigger
+              value="In progress"
+              onClick={() => onChange("In progress")}
+            >
+              In progress
+            </TabsTrigger>
+            <TabsTrigger
+              value="Completed"
+              onClick={() => onChange("Completed")}
+            >
+              Completed
+            </TabsTrigger>
+            <TabsTrigger
+              value="Long-Term"
+              onClick={() => onChange("Long-Term")}
+            >
+              Long-Term
+            </TabsTrigger>
+          </>
+        ) : (
+          <TabsTrigger 
+          value="value"
+          data-state={value === value? 'active' : ''}
+          >{value}</TabsTrigger>
+        )}
       </TabsList>
-      
     </Tabs>
   );
 }
