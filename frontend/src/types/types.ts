dc513f81b78938de3a7bbb0bc4f1ede915922e9f
Douglas Colin Moore <dougcolin@gmail.com>
2023-08-14 11:42:12 -0400

    refactor: Moved types to central src/types/types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -2,22 +2,84 @@
 
 //type for properties of data sent to backend when user registers or logs in via form
 export interface RegisterUserData {
-    name: string;
-    email: string;
-    password: string;
-  }
+  name: string;
+  email: string;
+  password: string;
+}
 
 //type for properties of data sent to backend when user registers or logs in via form
 export interface LoginUserData {
-    email: string;
-    password: string;
-  }
+  email: string;
+  password: string;
+}
 
 //Type for properties of the user data
 export interface User {
-    _id: string;
-    name: string;
-    email: string;
-    token: string;
-  }
+  _id: string;
+  name: string;
+  email: string;
+  token: string;
+}
 
+export interface Project {
+  _id?: string;
+  projectName: string;
+  client?: string; 
+  dueDate: Date;
+  status?: "In progress" | "Completed" | "Long-Term"; 
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface ProjectFormProps {
+  onFormSubmit: (state: boolean) => void; 
+}
+
+export interface ProjectFormType {
+  projectName: string;
+  client?: string;
+  dueDate: Date;
+  status?: "In progress" | "Completed" | "Long-Term";
+}
+
+export interface Task {
+  _id?: string;
+  projectName: string;
+  client?: string;
+  label:
+    | "General"
+    | "Sourcing"
+    | "Fabrication"
+    | "Finishing"
+    | "Shipping"
+    | "Repair"
+    | "Administrative"
+    | "Maintenance";
+  taskName: string;
+  taskDescription: string;
+  status: "To Do" | "In Progress" | "Done" | "Paused" | "Canceled";
+  priority: "Low" | "Medium" | "High" | "Urgent";
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface TaskFormProps {
+  onFormSubmit: (state: boolean) => void; 
+}
+export interface TaskFormType {
+  projectName: string;
+  client?: string;
+  label:
+    | "General"
+    | "Sourcing"
+    | "Fabrication"
+    | "Finishing"
+    | "Shipping"
+    | "Repair"
+    | "Administrative"
+    | "Maintenance";
+  taskName: string;
+  taskDescription: string;
+  status: "To Do" | "In Progress" | "Done" | "Paused" | "Canceled";
+  priority: "Low" | "Medium" | "High" | "Urgent";
+}
