4bf522bd7ae6936a7f98522f347cb7b78b85f2b4
Douglas Colin Moore <dougcolin@gmail.com>
2023-08-15 18:45:19 -0400

    feat:(tasks) Align frontend taskSchema with backend data structure

---
+++ frontend/src/validators/taskSchema.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+
+export const taskSchema = z
+  .object({
+    projectName: z.string().min(3).max(100),
+    client: z.string().min(3).max(150).optional(),
+    label: z.union([
+        z.literal("General"),
+        z.literal("Sourcing"),
+        z.literal("Fabrication"),
+        z.literal("Finishing"),
+        z.literal("Shipping"),
+        z.literal("Repair"),
+        z.literal("Administrative"),
+        z.literal("Maintenance"),
+    ]),
+    taskName: z.string().min(3).max(100),
+    taskDescription: z.string().min(3).max(300),
+    status: z.union([
+      z.literal("To Do"),
+      z.literal("In Progress"),
+      z.literal("Done"),
+      z.literal("Paused"),
+      z.literal("Canceled"),
+    ]),
+    priority: z.union([
+      z.literal("Low"),
+      z.literal("Medium"),
+      z.literal("High"),
+      z.literal("Urgent"),
+    ]),
+  })
+
