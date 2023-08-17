b674cfe01d3429bf11fddbd9e3b45830ec2a0610
Douglas Colin Moore <dougcolin@gmail.com>
2023-08-16 20:35:16 -0400

    refactor(FormDialog): Moved state and handler to separate hook

--- frontend/src/components/FormDialog.tsx
+++ frontend/src/components/FormDialog.tsx
@@ -17,15 +17,35 @@ interface FormDialogProps {
   formComponent: React.ReactNode;
 }
 
+
+// State and handler fucntion to close dialog from parent component:
+// const [isFormDialogOpen, setIsFormDialogOpen] = useState(false); //state for closing FormDialog upon succesfull form submission
+//handler for closing FormDialog child component when form is succesfully submitted.
+// const handleFormDialogClose = (state: boolean) => {
+//   setIsFormDialogOpen(state);
+// };
+//In addition, make sure the onSubmit of the form that is FOrmDialog's child has a matching fn signature to handleFormDialogClose
+//interface ChildFormProps {
+//  onFormSubmit: (state: boolean) => void;
+//}
+
 export default function FormDialog({
   isOpen,
   onFormSubmissionCloseDialog,
   title,
   description,
   formComponent,
-}: FormDialogProps) {
+}: FormDialogProps) 
+{
+  //debugging
+  // console.log("3) FormDialog isOpen:", isOpen)
   return (
-    <Dialog open={isOpen} onOpenChange={ onFormSubmissionCloseDialog }>
+    // debugging
+      <Dialog open={isOpen} onOpenChange={ onFormSubmissionCloseDialog }> 
+    {/* <Dialog open={isOpen} onOpenChange={(newState: boolean) => { 
+       console.log("6) onOpenChange triggered with state:", newState); 
+       onFormSubmissionCloseDialog(newState); 
+     }}> */}
       <DialogTrigger asChild>
         <Button variant="outline">
           {/* <PlusIcon className="mr-2 h-4 w-4" /> */}
