b26f003fe38787db29c6fe3d70567b4f0ce8a7d2
Douglas Colin Moore <dougcolin@gmail.com>
2023-08-16 20:13:46 -0400

    refactor(FormDialog): Moved state and handler to separate hook

---
+++ frontend/src/hooks/useFormDialogState.ts
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+
+
+export const useFormDialogState = (initialState: boolean) => {
+
+    const [isFormDialogOpen, setIsFormDialogOpen] = useState(initialState)
+    const handleFormDialogClose = (state: boolean) => setIsFormDialogOpen(state)
+    
+    return {isFormDialogOpen, handleFormDialogClose} 
+}
+
+
