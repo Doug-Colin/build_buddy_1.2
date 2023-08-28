cf09e7ddcea8219500383ed378c2ce624247affc
Douglas Colin Moore <dougcolin@gmail.com>
2023-08-28 09:15:00 -0400

    feat(notes): Add components for basic PlateUI rich text editor to src/components/plate-ui

---
+++ frontend/src/components/plate-ui/floating-toolbar.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { flip, offset } from '@floating-ui/react';
+import { PortalBody, useComposedRef } from '@udecode/plate-common';
+import {
+  useFloatingToolbar,
+  useFloatingToolbarState,
+} from '@udecode/plate-floating';
+import { FloatingToolbarState } from '@udecode/plate-floating/dist';
+
+import { cn } from '@/lib/utils';
+
+import { Toolbar, ToolbarProps } from './toolbar';
+
+export interface FloatingToolbarProps extends ToolbarProps {
+  state?: FloatingToolbarState;
+}
+
+const FloatingToolbar = React.forwardRef<
+  React.ElementRef<typeof Toolbar>,
+  FloatingToolbarProps
+>(({ state, children, ...props }, componentRef) => {
+  const floatingToolbarState = useFloatingToolbarState({
+    ...state,
+    floatingOptions: {
+      placement: 'top',
+      middleware: [
+        offset(12),
+        flip({
+          padding: 12,
+          fallbackPlacements: [
+            'top-start',
+            'top-end',
+            'bottom-start',
+            'bottom-end',
+          ],
+        }),
+      ],
+      ...state?.floatingOptions,
+    },
+  });
+
+  const {
+    ref: floatingRef,
+    props: rootProps,
+    hidden,
+  } = useFloatingToolbar(floatingToolbarState);
+
+  const ref = useComposedRef<HTMLDivElement>(componentRef, floatingRef);
+
+  if (hidden) return null;
+
+  return (
+    <PortalBody>
+      <Toolbar
+        ref={ref}
+        className={cn(
+          'absolute z-50 whitespace-nowrap border border-stone-200 bg-white px-1 opacity-100 shadow-md dark:border-stone-800 dark:bg-stone-950'
+        )}
+        {...rootProps}
+        {...props}
+      >
+        {children}
+      </Toolbar>
+    </PortalBody>
+  );
+});
+FloatingToolbar.displayName = 'FloatingToolbar';
+
+export { FloatingToolbar };
