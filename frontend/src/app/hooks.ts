2a4e7639dffe972510947287225671a78e9ccc54
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-01 09:13:54 -0400

    chore(Redux hooks.ts): Clean up code by removing comments, making comments more concise, updating comments etc

--- frontend/src/app/hooks.ts
+++ frontend/src/app/hooks.ts
@@ -1,27 +1,16 @@
+// Utils/typed wrappers specific to Redux & Redux Toolkit
 import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios';
   
 
-//Redux in TS: Use these pre-typed functions throughout app instead of plain `useDispatch` and `useSelector.
+// Typed wrappers for useDispatch & useSelector
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 
-
+// Typed wrapper for createAsyncThunk to handle reject values (reduce redundant code).
 export const createTypedAsyncThunk = createAsyncThunk.withTypes<{
     rejectValue: string;
   }>();
-
-
-//function to minimize AxiosError type checking code in thunk actions
-export function getErrorMessage(error: unknown): string {
-    if (axios.isAxiosError<{ error?: { message: string } }>(error)) {
-      return (
-        error.response?.data?.error?.message || error.message || error.toString()
-      );
-    }
-    return "An unknown error occurred.";
-  }
