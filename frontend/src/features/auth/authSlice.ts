f9412b769b91f58d8ef11de7828cb19bc6309346
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-02 10:35:40 -0400

    refactor(authSlice.ts): Clean up code by removing/updating/making more concise comments
    update import statement for getErrorMessage axiol Util moved to own file

--- frontend/src/features/auth/authSlice.ts
+++ frontend/src/features/auth/authSlice.ts
@@ -1,14 +1,13 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import authService from "./authService";
 import { User, LoginUserData, RegisterUserData } from "@/types/types";
-import { getErrorMessage, createTypedAsyncThunk } from "@/app/hooks";
-//May need for selector functions, as per Redux+TS docs
-//import type { RootState } from "../../app/store";
+import { createTypedAsyncThunk } from "@/app/hooks";
+import { getErrorMessage } from "@/lib/axiosUtils";
 
-//get user from local storage; becomes part of global redux store upon initialization
-const user: User = JSON.parse(localStorage.getItem('user') || 'null')
+//Get user from local storage; becomes part of global redux store upon initialization
+const user: User = JSON.parse(localStorage.getItem("user") || "null");
 
-//types for the initialState
+//Types for initialState
 export interface AuthState {
   user: User | null;
   isError: boolean;
@@ -17,34 +16,29 @@ export interface AuthState {
   message: string;
 }
 
-// Check Redux toolkit if experiencing any authState issues.
+// Initial state
 const initialState: AuthState = {
-  user: user, 
-  isError: false, 
-  isSuccess: false, 
-  isLoading: false, // For loading spinner.
+  user: user,
+  isError: false,
+  isSuccess: false,
+  isLoading: false, // For loading animation (spinner etc).
   message: "",
 };
 
-
-/**
- * Thunk action to register user; handles Axios-specific and general errors with help of getErrorMessage()
- */
-export const register = createTypedAsyncThunk<
-  User,
-  RegisterUserData
->("auth/register", async (user, thunkAPI) => {
-  try {
-    return await authService.register(user);
-  } catch (error: unknown) {
-    const message = getErrorMessage(error);
-    return thunkAPI.rejectWithValue(message);
+//Thunk action to register user; handles Axios-specific and general errors with help of getErrorMessage()
+export const register = createTypedAsyncThunk<User, RegisterUserData>(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error: unknown) {
+      const message = getErrorMessage(error);
+      return thunkAPI.rejectWithValue(message);
+    }
   }
-});
+);
 
-/**
- * Thunk action to login user; handles Axios-specific and general errors with help of getErrorMessage()
- */
+//Thunk action to login user; handles Axios-specific and general errors with help of getErrorMessage()
 export const login = createAsyncThunk<
   User,
   LoginUserData,
@@ -58,21 +52,18 @@ export const login = createAsyncThunk<
   }
 });
 
-/**
- * Thunk action to logout user
- */
+//Thunk action to logout user
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
 
-// Create a slice with typed state and payload.
+// Create slice with typed state & payload
 export const authSlice = createSlice({
   name: "auth",
   initialState,
 
-  // Reducers for synchronous actions.
+  // reducers - synchronous actions.
   reducers: {
-    // 'Reset' state properties, retaining the user.
     reset: (state: AuthState) => {
       state.isLoading = false;
       state.isSuccess = false;
@@ -81,27 +72,27 @@ export const authSlice = createSlice({
     },
   },
 
-  // Handle asynchronous actions using extraReducers.
+  // extraReducers - async actions
   extraReducers: (builder) => {
     builder
-      // Handle actions fired upon register submission.
+      //actions fired upon register submission.
       .addCase(register.pending, (state) => {
         state.isLoading = true;
       })
       .addCase(register.fulfilled, (state, action) => {
         state.isLoading = false;
         state.isSuccess = true;
-        state.user = action.payload
+        state.user = action.payload;
       })
 
       .addCase(register.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
-        state.message = action.payload as string
+        state.message = action.payload as string;
         state.user = null;
       })
 
-      // Handle actions fired upon login submission.
+      //actions fired upon login submission.
       .addCase(login.pending, (state) => {
         state.isLoading = true;
       })
@@ -117,14 +108,12 @@ export const authSlice = createSlice({
         state.user = null;
       })
 
-      // Handle logout action.
+      // handle logout action.
       .addCase(logout.fulfilled, (state) => {
         state.user = null;
       });
   },
 });
 
-//export authSlice so we can import it in src/app/store.js
-//reducers like reset must be exported from authSlice.actions
 export const { reset } = authSlice.actions;
 export default authSlice.reducer;
