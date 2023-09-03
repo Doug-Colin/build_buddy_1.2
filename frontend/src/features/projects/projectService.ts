8b6ada35a295bfed686e20874e3e762d1a2813ee
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-03 13:31:50 -0400

    refactor(projectService): Update code to use centralized config initialization for Axios requests via getConfig()

--- frontend/src/features/projects/projectService.ts
+++ frontend/src/features/projects/projectService.ts
@@ -1,58 +1,43 @@
 import axios from "axios";
+import { Project } from "@/types/types";
+import { getConfig } from "@/lib/axiosUtils";
 
 const API_URL = "/api/projects/";
-import { Project } from "@/types/types";
-//define type for properties or data of individual projects
+
+// HTTP requests to the backend made via appropriate Axios methods
+// Auth token is sent as a Bearer token in the config arg of the Axios req. method via getConfig()
 
 
-//Request to create new project
-//send token as Bearer token
+//Create project
 const createProject = async (project: Project, token: string) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  //debugging
-  console.log("Sending request with data:", project, "and config:", config);
-
-  //respond to req with config, which has the token
-  const response = await axios.post(API_URL, project, config);
-
+  const response = await axios.post(API_URL, project, getConfig(token))
   return response.data;
 };
 
-//Request to get user projects
+//Get user's projects
 const getProjects = async (token: string) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  //respond to req with config, which has the token
-  const response = await axios.get(API_URL, config);
-
+  const response = await axios.get(API_URL, getConfig(token));
   return response.data;
 };
 
-// Request to Update a Project
+// Update Project
 const updateProject = async (
   projectId: string,
   updatedProject: Partial<Project>,
   token: string
 ) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`, //Prepend 'Bearer' to the raw token
-    },
-  };
-
-  const response = await axios.put(API_URL + projectId, updatedProject, config);
-
+  const response = await axios.put(
+    API_URL + projectId,
+    updatedProject,
+    getConfig(token)
+  );
   return response.data;
 };
 
-// //Request to duplicate project
+/* Duplicate project 
+    -Initializes a copy of the original project locally- "Copy" is appended to projectName,  and status of copiedProject is set to "In progress".
+    -Deletes the copiedProject's _id, createdAt, and updatedAt properties so they can be generated anew.
+    -Sends the copiedProject in a post req to the backend, where Mongoose will create _id, createdAt, and updatedAt properties as if it was a new project.*/
 const duplicateProject = async (originalProject: Project, token: string) => {
   const copiedProject: Project = {
     ...originalProject,
@@ -60,27 +45,13 @@ const duplicateProject = async (originalProject: Project, token: string) => {
     status: "In progress",
   };
   delete copiedProject._id, copiedProject.createdAt, copiedProject.updatedAt;
-
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  const response = await axios.post(API_URL, copiedProject, config);
-
+  const response = await axios.post(API_URL, copiedProject, getConfig(token));
   return response.data;
 };
 
-//Request to delete project
+//Delete project
 const deleteProject = async (projectId: string, token: string) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  //respond to req with config, which has the token
-  const response = await axios.delete(API_URL + projectId, config);
-
+  const response = await axios.delete(API_URL + projectId, getConfig(token));
   return response.data;
 };
 
