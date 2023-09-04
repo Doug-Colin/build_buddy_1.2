1a933e583a730ef949aee87fc862d6efaa9409fb
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-04 10:21:33 -0400

    feat(Layout): Implement centralized Layout component to reduce repetitive calls of Header, MainNav, and Search components on feature pages

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,28 @@
+//Layout to centralize rendering of Header, MainNav, and Search on all feature pages
+
+import Header from "@/components/Header";
+import { MainNav } from "@/pages/dashboard-page/components/main-nav";
+import { Search } from "@/pages/dashboard-page/components/search";
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <MainNav className="mx-3" />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+            </div>
+          </div>
+        </div>
+        {/* For applying global styling/layout rules to all children components, if necessary */}
+        <div className="">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
