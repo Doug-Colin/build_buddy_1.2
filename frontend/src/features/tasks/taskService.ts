a27875728255a92203220616ca23803c6ba25c56
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-04 08:07:04 -0400

    refactor(taskService): Update code to use centralized config initialization for Axios requests via getConfig()

--- frontend/src/features/tasks/taskService.ts
+++ frontend/src/features/tasks/taskService.ts
@@ -1,40 +1,21 @@
 import axios from "axios";
 import { Task } from "@/types/types";
+import { getConfig } from "@/lib/axiosUtils";
 
 const API_URL = "/api/tasks/";
 
-//define type for properties or data of individual tasks
-
-
-
+// HTTP requests to the backend made via appropriate Axios methods
+// Auth token is sent as a Bearer token in the config arg of the Axios req. method via getConfig()
 
 //Request to create new task
-//send token as Bearer token
 const createTask = async (task: Task, token: string) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  //debugging
-  console.log("Sending request with data:", task, "and config:", config);
-
-  //respond to req with config, which has the token
-  const response = await axios.post(API_URL, task, config);
-
+  const response = await axios.post(API_URL, task, getConfig(token));
   return response.data;
 };
 
 //Req to get user tasks
 const getTasks = async (token: string) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  //respond to req with config, which has the token
-  const response = await axios.get(API_URL, config);
-
+  const response = await axios.get(API_URL, getConfig(token));
   return response.data;
 };
 
@@ -44,51 +25,33 @@ const updateTask = async (
   updatedTask: Partial<Task>,
   token: string
 ) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`, //Prepend 'Bearer' to the raw token
-    },
-  };
-
-  const response = await axios.put(API_URL + taskId, updatedTask, config);
-
+  const response = await axios.put(API_URL + taskId, updatedTask, getConfig(token));
   return response.data;
 };
 
-//Req to duplicate task
+/* Req to duplicate task 
+    -Initializes a copy of the original task locally- "Copy" is appended to taskName,  and status of copiedTask is set to "In progress".
+    -Deletes the copiedTask's _id, createdAt, and updatedAt properties so they can be generated anew.
+    -Sends the copiedTask in a post req to the backend, where Mongoose will create _id, createdAt, and updatedAt properties as if it was a new task.*/
 const duplicateTask = async (originalTask: Task, token: string) => {
   const copiedTask: Task = {
     ...originalTask,
     taskName: `${originalTask.taskName} (Copy)`,
-
     //populate
     status: "In Progress",
     priority: "Low"
   };
   delete copiedTask._id, copiedTask.createdAt, copiedTask.updatedAt;
-
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  const response = await axios.post(API_URL, copiedTask, config);
-
+  const response = await axios.post(API_URL, copiedTask, getConfig(token));
   return response.data;
 };
 
 //Req to delete task
 const deleteTask = async (taskId: string, token: string) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  //respond to req with config, which has the token
-  const response = await axios.delete(API_URL + taskId, config);
-
+  const response = await axios.delete(API_URL + taskId, getConfig(token));
   return response.data;
 };
+
 
 const taskService = {
   createTask,
