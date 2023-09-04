e3e2c16af28d59e557f22c17d91b6ae9c507f522
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-04 08:35:38 -0400

    chore(assetUtils): Clean up code by removing/updating/making more concise comments

--- frontend/src/lib/assetUtils.ts
+++ frontend/src/lib/assetUtils.ts
@@ -1,20 +1,5 @@
-
 type Theme = 'light' | 'dark'
 
 export function darkOrLightIconPath(featureName: string, theme: 'light' | 'dark'): string {
     return `@assets/${featureName}${theme.charAt(0).toUpperCase() + theme.slice(1)}Mode.svg`
 }
-
-
-/*
-Note:
-When we say that utility functions should not depend on the global state, we mean the functions themselves should not retrieve or directly interact with global state. They can, however, take parameters that might be derived from a global state when called.
-
-The function above doesn't directly call useTheme(). Instead, you pass the theme as an argument to the function. The fact that the theme might come from a global state (through React Context in your application) when you call it function is okay.
-
-Whereas the following version of this function would be 'impure' and not best practice: 
-export function getFeatureIconPathImpure(featureName: string): string {
-  const theme = useTheme();  // Directly calling a hook to get global state
-  return `/path/to/icons/${featureName}${theme.charAt(0).toUpperCase() + theme.slice(1)}Mode.png`;
-}
-*/
