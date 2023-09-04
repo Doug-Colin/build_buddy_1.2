3a35dfd9098cb9e1f75dbbe45217ed5a3dca1d24
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-04 09:15:37 -0400

    chore(assetUtils): Clean up code by removing/updating/making more concise comments

--- frontend/src/lib/assetUtils.ts
+++ frontend/src/lib/assetUtils.ts
@@ -1,3 +1,5 @@
+//Makes custom icons light or dark by returning the appropriate file, depending on the user's current selected theme 
+
 type Theme = 'light' | 'dark'
 
 export function darkOrLightIconPath(featureName: string, theme: 'light' | 'dark'): string {
