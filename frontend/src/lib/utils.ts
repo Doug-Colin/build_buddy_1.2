d507f96ced0ba4ffe02535ab9149a64925045480
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-04 08:56:08 -0400

    chore(utils): Clean up code by removing/updating/making more concise comments

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,27 +1,11 @@
+//Utils for styling shadcn/ui and Plate UI components
+
+//Conditionally concatenates classNames by combining them into single string & filtering out falsy values.
 import { type ClassValue, clsx } from "clsx"
+
+//Merges multiple instances of same Tailwind class into single optimized class.
 import { twMerge } from "tailwind-merge"
 
-/*
---------------- Using Utils and TS ---------------
-
-  Utils, or utility functions, are reusable functions that are needed in various places of your application.
-
-  The criteria for what should or shouldn't be a utils function is:
-
-  Reusability: primary reason for moving a function here is that it's logic that is reused.
-
-  No Dependencies on App State: Utility functions should be "pure" and not dependent on the global app state (for ex. no reading from Redux store)
-
-  Simple & testable: function with predictable outputs given same inputs.
-  --------------- How to use your utils ---------------
-
-  Simply import the util at the top of your component from the correct path, and call it in your component function.
-
-  --------------- Advantages of utils ---------------
-
-  The advantages of utils are those common to any advantageous programming approach- they help keep code DRY, organized, readable, testable, and maintainable.
-*/
- 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
