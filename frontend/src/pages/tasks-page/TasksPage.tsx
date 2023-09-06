1e4edf5ee3412c17486d392561403668c67091f0
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-06 08:32:51 -0400

    refactor(TasksPage): integrate Layout component to streamline UI rendering

--- frontend/src/pages/tasks-page/TasksPage.tsx
+++ frontend/src/pages/tasks-page/TasksPage.tsx
@@ -1,41 +1,20 @@
-
-import { useEffect } from "react";
+import Layout from "@/components/Layout";
 import { useAuthCheck } from "@/hooks/useAuthCheck";
-import Header from "@/components/Header";
-import { MainNav } from "@/pages/dashboard-page/components/main-nav";
-import { Search } from "@/pages/dashboard-page/components/search";
 import { columns } from "@/pages/tasks-page/components/tasks-table/columns";
 import { DataTable } from "@/pages/tasks-page/components/tasks-table/data-table";
-import { getTasks } from "@/features/tasks/taskSlice";
-import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import { useAppSelector } from "@/app/hooks";
 
 export default function TasksPage() {
+  // !user redirects to LandingPage
   useAuthCheck();
-  const dispatch = useAppDispatch()
-  const tasks = useAppSelector((state) => state.tasks.tasks) 
 
-
-  useEffect(() => {
-    dispatch(getTasks());
-  }, [dispatch]);
+  const tasks = useAppSelector((state) => state.tasks.tasks);
 
   return (
-    <>
-      <Header />
-      <div className="hidden flex-col md:flex">
-        <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <MainNav className="mx-3" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-            </div>
-          </div>
-        </div>
-
-        <div className="">
-             <DataTable columns={columns} data={tasks} />
-          </div>
+    <Layout>
+      <div className="">
+        <DataTable columns={columns} data={tasks} />
       </div>
-    </>
+    </Layout>
   );
 }
