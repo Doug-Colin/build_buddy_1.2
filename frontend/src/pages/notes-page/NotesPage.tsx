502300077256b778652aaa77b6f1f7a2244f7e7d
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-07 07:02:46 -0400

    refactor(NotesPage): integrate Layout component to streamline UI rendering

--- frontend/src/pages/notes-page/NotesPage.tsx
+++ frontend/src/pages/notes-page/NotesPage.tsx
@@ -1,51 +1,28 @@
-import Header from "@/components/Header";
-import { MainNav } from "@/pages/dashboard-page/components/main-nav";
-import { Search } from "@/pages/dashboard-page/components/search";
 import { TooltipProvider } from "@/components/plate-ui/tooltip";
-import Editor from "./plate/editor";
+import Editor from "@/pages/notes-page/plate/editor";
 import { useAuthCheck } from "@/hooks/useAuthCheck";
-
+import Layout from "@/components/Layout";
 
 export default function ProjectsPage() {
- 
-
   // !user redirects to LandingPage
-  useAuthCheck()
-
+  useAuthCheck();
 
   return (
-    <>
-      <Header />
-
-      <div className="hidden flex-col md:flex">
-        
-        <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <MainNav className="mx-3" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-            </div>
+    <Layout>
+      <TooltipProvider
+        disableHoverableContent
+        delayDuration={500}
+        skipDelayDuration={0}
+      >
+        <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+          <div className="flex max-w-[980px] flex-col items-start gap-2">
+            {/* text and buttons here */}
           </div>
-        </div>
-
-        <TooltipProvider
-            disableHoverableContent
-            delayDuration={500}
-            skipDelayDuration={0}
-          >
-            <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-              <div className="flex max-w-[980px] flex-col items-start gap-2">
-                {/* Your text and buttons here */}
-              </div>
-              <div className="max-w-[1336px] rounded-lg border bg-background shadow">
-                <Editor />
-              </div>
-            </section>
-          </TooltipProvider>
-          
-       </div>
-
-      
-    </>
+          <div className="max-w-[1336px] rounded-lg border bg-background shadow">
+            <Editor />
+          </div>
+        </section>
+      </TooltipProvider>
+    </Layout>
   );
 }
