1f2a8fd331dbae807cfd1431ece9c501c295f78a
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-19 08:20:48 -0400

    feat(notes): Add '/api/notes' route to server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/projects', require('./routes/projectRoutes'));
+app.use('/api/notes', require('./routes/noteRoutes'));
 
 // Overwrite default express error handler
 app.use(errorHandler);
