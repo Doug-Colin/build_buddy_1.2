2a92734b379c151410cef07967347e24b6d6c1ef
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-24 17:16:32 -0400

    feat(notes): Update noteSchema to reflect basic/initial version of NoteForm.

---
+++ frontend/src/validators/noteSchema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+
+export const noteSchema = z.object({
+  noteTitle: z.string().min(3).max(50),
+  noteLabel: z.union([
+    z.literal('Project'),
+    z.literal('Task'),
+    z.literal('Client'),
+    z.literal('General'),
+  ]),
+  projectId: z.string().min(3).max(150).optional(), 
+  taskId: z.string().min(3).max(150).optional(), 
+  client: z.string().min(3).max(150).optional(),
+})
+
