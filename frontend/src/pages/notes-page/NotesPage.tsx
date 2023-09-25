c3013b6781bab14a994ee64136ab19aef138e5c4
Douglas Colin Moore <dougcolin@gmail.com>
2023-09-25 08:09:59 -0400

    feat(notes): Add NoteForm and update NotesPage for basic/initial version of NoteForm.

--- frontend/src/pages/notes-page/NotesPage.tsx
+++ frontend/src/pages/notes-page/NotesPage.tsx
@@ -1,13 +1,16 @@
 import { TooltipProvider } from '@/components/plate-ui/tooltip'
-import Editor from '@/pages/notes-page/plate/editor'
+import Editor from '@/pages/notes-page/components/editor'
 import { useAuthCheck } from '@/hooks/useAuthCheck'
 import Layout from '@/components/Layout'
+import FormDialog from '@/components/FormDialog'
+import { useFormDialogState } from '@/hooks/useFormDialogState'
+import NoteForm from './components/NoteForm'
 
-
-
-export default function ProjectsPage() {
+export default function NotesPage() {
   // !user redirects to LandingPage
   useAuthCheck()
+
+  const { isFormDialogOpen, handleFormDialogClose } = useFormDialogState(false)
 
   return (
     <Layout>
@@ -18,7 +21,13 @@ export default function ProjectsPage() {
       >
         <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
           <div className="flex max-w-[980px] flex-col items-start gap-2">
-            {/* text and buttons here */}
+            <FormDialog
+              title="Create Note"
+              description="Fill out the form to create a new Note. Click save when you're done."
+              isOpen={isFormDialogOpen}
+              onFormSubmissionCloseDialog={handleFormDialogClose}
+              formComponent={<NoteForm onFormSubmit={handleFormDialogClose} />}
+            />
           </div>
           <div className="max-w-[1336px] rounded-lg border bg-background shadow">
             <Editor />
---
+++ frontend/src/pages/notes-page/components/NoteForm.tsx
@@ -0,0 +1,165 @@
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '@/app/hooks'
+import { useForm } from 'react-hook-form'
+import { RootState } from '@/app/store'
+import { getProjects } from '@/features/projects/projectSlice'
+import { getTasks } from '@/features/tasks/taskSlice'
+import { createNote } from '@/features/notes/noteSlice'
+
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { noteSchema } from '@/validators/noteSchema'
+// import { NoteFormProps, NoteFormType, Project } from '@/types/types'
+import { Project, NoteFormProps, NoteDTO } from '@/types/types'
+
+
+import { Button, Input } from '@/components/ui'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+
+type Input = z.infer<typeof noteSchema>
+
+
+
+export default function NoteForm({ onFormSubmit }: NoteFormProps) {
+  const dispatch = useAppDispatch()
+  const projects = useAppSelector((state: RootState) => state.projects.projects)
+
+  const noteLabelOptions: string[] = [
+    'Project','Task', 'Client', 'General'
+  ]
+
+  function onSubmit(data: NoteDTO ) {
+    dispatch(createNote(data))
+    console.log(`Here is entered form data: ${data}`)
+    onFormSubmit(false)
+  }
+
+  const form = useForm<Input>({
+    resolver: zodResolver(noteSchema),
+    defaultValues: {},
+  })
+
+  console.log(form.watch())
+
+  useEffect(() => {
+    dispatch(getProjects())
+  }, [dispatch])
+
+  useEffect(() => {
+    dispatch(getTasks())
+  }, [dispatch])
+
+  return (
+    //centered
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* Input for Note Title */}
+        <FormField
+            control={form.control}
+            name="noteTitle"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Note Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Title" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          
+          {/* input for projectName so note can be associated with project*/}
+          <FormField
+            control={form.control}
+            name="projectId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Parent Project</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Choose the parent project of this note" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {projects.map((project: Project) => {
+                      return (
+                        <SelectItem
+                          key={project._id}
+                          value={project._id}
+                        >
+                          {project.projectName}
+                        </SelectItem>
+                      )
+                    })}
+                  </SelectContent>
+                </Select>
+                <FormDescription>
+                  Attach notes to projects so you can duplicate them along with
+                  the project.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* input for note's label*/}
+          <FormField
+            control={form.control}
+            name="noteLabel"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Label</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Choose note label" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {noteLabelOptions.map((label, index) => {
+                      return (
+                        <SelectItem key={index} value={label}>
+                          {label}
+                        </SelectItem>
+                      )
+                    })}
+                  </SelectContent>
+                </Select>
+                <FormDescription>
+                  Label your notes to help keep them organized.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
