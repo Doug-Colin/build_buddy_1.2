0b5d13f67209acd06c22dec7b8059c6de21aa7be
Douglas Colin Moore <dougcolin@gmail.com>
2023-11-11 09:02:21 -0500

    feat(notes): Update types for new Editor functionality (saving, loading upon selection).

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -101,6 +101,8 @@ export interface NoteFormProps {
 
 export interface NoteDTO {
   noteTitle: string
+  
+  //noteContent?: null | string
   noteContent?: null | string
   noteLabel: 'Project' | 'Task' | 'Client' | 'General'
   projectId?: string
