d92a2078dbfd0c0df575ccc564bfe6c5a7a3e066
Douglas Colin Moore <dougcolin@gmail.com>
2023-11-14 09:38:44 -0500

    feat(notes) Create modular editor component for use across application, with appropriate props for for use in parent to set initial editor content, tracking and save changes via onChange, & ref.

---
+++ frontend/src/pages/notes-page/components/ModularEditor.tsx
@@ -0,0 +1,57 @@
+// Plate dependencies, components, plugins, and types.
+import { Plate } from '@udecode/plate-common'
+import { Editor } from '@/components/plate-ui/editor'
+import { FixedToolbar } from '@/components/plate-ui/fixed-toolbar'
+import { FixedToolbarButtons } from '@/components/plate-ui/fixed-toolbar-buttons'
+import { FloatingToolbar } from '@/components/plate-ui/floating-toolbar'
+import { FloatingToolbarButtons } from '@/components/plate-ui/floating-toolbar-buttons'
+import plugins from '../plugins/plugins'
+import { MyValue } from '@/types/plate-types'
+
+interface ModularEditorProps {
+  editorRef: any
+  initialContent: MyValue
+  editorKey?: string
+  //currentNote: Note,
+  onChange: (initialContent: MyValue) => void
+}
+
+export default function ModularEditor({
+  editorRef,
+  editorKey,
+  initialContent,
+  onChange,
+}: ModularEditorProps) {
+  /* 
+Alternate note saving handler via Slate docs; checks for Abstract Syntax Tree (AST) changes & dispatches updateNote() accordingly.
+*/
+/*
+const saveAstContentChange = (newValue: MyValue) => {
+     const isAstChange = newValue.some(n => n.type !== 'selection');
+     if (isAstChange && currentNote?._id) {
+       const updatedContent = JSON.stringify(newValue);
+       dispatch(updateNote({
+         noteId: currentNote._id,
+         updatedNote: { noteContent: updatedContent },
+       }));
+    }
+*/
+
+  return (
+    <Plate
+      key={editorKey}
+      editorRef={editorRef}
+      initialValue={initialContent}
+      plugins={plugins}
+      onChange={onChange}
+    >
+      <FixedToolbar>
+        <FixedToolbarButtons />
+      </FixedToolbar>
+      <Editor />
+      <FloatingToolbar>
+        <FloatingToolbarButtons />
+      </FloatingToolbar>
+    </Plate>
+  )
+}
