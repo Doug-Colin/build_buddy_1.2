72618a093599a6abc879065a1982cc82b46db50a
Douglas Colin Moore <102709219+Doug-Colin@users.noreply.github.com>
2024-02-01 08:30:56 -0500

    feat(responsive-layout) update responsive table page.

--- frontend/src/pages/responsive-table-view/ResponsiveTablePage.tsx
+++ frontend/src/pages/responsive-table-view/ResponsiveTablePage.tsx
@@ -1,55 +1,5 @@
+import ResponsiveTable from "./components/ResponsiveTable"
 
-
-//Currently a mockup 
-
-import {
-    Table, TableBody, TableCell, TableHead, TableHeader, TableRow,
-  } from "@/components/ui/table";
-  
-  export default function ResponsiveTable() {
-
-
-    // these are supposed to be the link objects, we'll get there later
-    //.const links = [...] 
-
-    // interface Link {
-    //     id: string;
-    //     name: string;
-    //     viewCount: string;
-    // }
-
-    const links = [{
-        id: "1",
-        name: "Google",
-        viewCount: "100"
-    }
-    ] 
-  
-    return (
-      <div className="w-full sm:p-4">
-        <h2 className="p-4">All links</h2>
-        <div className="rounded-md sm:border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="font-medium">Name</TableHead>
-                <TableHead className="font-medium">Link</TableHead>
-                <TableHead className="font-medium">Views</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {links ? (
-                links.map((link) => (
-                  <TableRow key={link.id}>
-                    <TableCell>{link.name}</TableCell>
-                    <TableCell>{link.id}</TableCell>
-                    <TableCell>{link.viewCount}</TableCell>
-                  </TableRow>
-                ))
-              ) : null}
-            </TableBody>
-          </Table>
-        </div>
-      </div>
-    );
-  }
+export default function ResponsiveTablePage() {
+  return <ResponsiveTable />
+}
