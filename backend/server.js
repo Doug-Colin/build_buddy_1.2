9cc0e40e190ae11a9cbc955dcec3ddc929db093a
Douglas Colin Moore <102709219+Doug-Colin@users.noreply.github.com>
2024-02-28 07:08:07 -0500

    (deployment) Update server.js for cloud deployment on Adaptable

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const port = process.env.port || 5050;
+const port = process.env.PORT || 5050;
 
 connectDB();
 
@@ -23,15 +23,17 @@ app.use('/api/notes', require('./routes/noteRoutes'));
 // Overwrite default express error handler
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Listen on all available IP addresses (necesessary for cloud deployment)
+app.listen(port, '0.0.0.0', () =>
+  console.log(`Server started on port ${port}`)
+);
 
 // Code for serving the frontend directly from the backend in production mode.
 // - Use if hosting frontend and backend together.
 // - Ensure frontend is built and the path (../frontend/build) matches the location of built files.
 // - For separate hosting of frontend and backend, this code can be ignored.
-// - Adjust paths if using a tool other than Create React App for frontend build.
+// - Adjust paths for tool used in frontend build (this case Vite).
 
-/*
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
@@ -43,4 +45,3 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
 }
-*/
