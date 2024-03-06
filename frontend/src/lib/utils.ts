5bcd5cda00c12e0140b79167b2e75826b2506976
Douglas Colin Moore <102709219+Doug-Colin@users.noreply.github.com>
2024-03-06 08:28:36 -0500

    (deployment) Format code in src/lib dir to prepare for deployment

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,11 +1,11 @@
 //Utils for styling shadcn/ui and Plate UI components
 
-  //Conditionally concatenates classNames by combining them into single string & filtering out falsy values.
-  import { type ClassValue, clsx } from 'clsx'
+//Conditionally concatenates classNames by combining them into single string & filtering out falsy values.
+import { type ClassValue, clsx } from 'clsx'
 
-  //Merges multiple instances of same Tailwind class into single optimized class.
-  import { twMerge } from 'tailwind-merge'
+//Merges multiple instances of same Tailwind class into single optimized class.
+import { twMerge } from 'tailwind-merge'
 
-  export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-  }
+}
