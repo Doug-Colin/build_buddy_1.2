d76f49bd15c37ba2da81c597144c4d7ead6eff40
Douglas Colin Moore <102709219+Doug-Colin@users.noreply.github.com>
2024-03-24 11:54:15 -0400

    (deployment) Adjust path in server.js to match dist dir for build

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,11 @@ app.listen(port, '0.0.0.0', () =>
 // - Adjust paths for tool used in frontend build (this case Vite).
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
     )
   );
 } else {
