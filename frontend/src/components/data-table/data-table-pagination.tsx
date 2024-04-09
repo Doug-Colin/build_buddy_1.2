9fa9114e629471fc00353f9c626b1de4ed3dcd6b
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2024-04-09 12:19:07 +0200

    (style) Add responsive styles to make TasksDataTable mobile-responsive.

--- frontend/src/components/data-table/data-table-pagination.tsx
+++ frontend/src/components/data-table/data-table-pagination.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -23,14 +24,18 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between px-2">
-      <div className="flex-1 text-sm text-muted-foreground">
+    // Hidding to debugg CSS
+    <div className="hidden flex items-center justify-between px-2">
+    {/* <div className="flex items-center justify-between px-2"> */}
+      <div className="flex-1 sm:text-xs md:text:sm text-muted-foreground">
+      {/* <div className="flex-1 text-sm text-muted-foreground"> */}
         {table.getFilteredSelectedRowModel().rows.length} of{' '}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
-          <p className="text-sm font-medium">Rows per page</p>
+          <p className="sm:text-xs md:text:sm font-medium">Rows per page</p>
+          {/* <p className="text-sm font-medium">Rows per page</p> */}
           <Select
             value={`${table.getState().pagination.pageSize}`}
             onValueChange={(value) => {
