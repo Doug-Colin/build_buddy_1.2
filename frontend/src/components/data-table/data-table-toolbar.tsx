5a652ae24fb0e5a6061968dc4e50673fa6c1fc54
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2024-04-17 08:13:20 +0200

    (feat) Add lucide-react search icon to tasks search bar

--- frontend/src/components/data-table/data-table-toolbar.tsx
+++ frontend/src/components/data-table/data-table-toolbar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Cross2Icon } from '@radix-ui/react-icons'
+import { Search }  from 'lucide-react'
 import { Table } from '@tanstack/react-table'
 
 import { Button } from '@/components/ui/button'
@@ -16,30 +17,28 @@ export function DataTableToolbar<TData>({
   const isFiltered = table.getState().columnFilters.length > 0
 
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex flex-1 items-center space-x-2">
-        <Input
-          placeholder="Filter tasks..."
-          value={
-            (table.getColumn('taskDescription')?.getFilterValue() as string) ??
-            ''
-          }
-          onChange={(event) =>
-            table
-              .getColumn('taskDescription')
-              ?.setFilterValue(event.target.value)
-          }
-          // Got rid of w-150, trying to find width that is making <th> not responsive
-          // className="h-8 w-[150px] lg:w-[200px]"
-          className="h-8"
-        />
+    <div className="flex items-center justify-between">     
+      <div className="relative ml-auto flex-1 md:grow-0">
+      <Search  />
+            <Input
+              type="search"
+              placeholder="Search Tasks..."
+              value={
+                (table.getColumn('taskDescription')?.getFilterValue() as string) ?? ''
+              }
+              onChange={(event) =>
+                table
+                  .getColumn('taskDescription')
+                  ?.setFilterValue(event.target.value)
+              }
+              className="w-[160px] h-8 rounded-lg bg-background pl-8 lg:w-[336px]"
+            /> 
         {isFiltered && (
           <Button
             variant="ghost"
             onClick={() => table.resetColumnFilters()}
             className="h-8 px-2 lg:px-3"
           >
-            Reset
             <Cross2Icon className="ml-2 h-4 w-4" />
           </Button>
         )}
