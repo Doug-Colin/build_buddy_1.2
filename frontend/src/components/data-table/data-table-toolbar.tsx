4312ca01cf4dc148f44874cf52d618957697a5af
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2024-04-20 10:48:16 +0200

    (style) Move Duplicate buttons from DataTableRowActions to DataTableToolbar so they can more easily be styled responsive

--- frontend/src/components/data-table/data-table-toolbar.tsx
+++ frontend/src/components/data-table/data-table-toolbar.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 import { Cross2Icon } from '@radix-ui/react-icons'
-import { Search, File, PlusCircle }  from 'lucide-react'
+import { Search, File, PlusCircle } from 'lucide-react'
 import { Table } from '@tanstack/react-table'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { DataTableViewOptions } from '@/components/data-table/data-table-view-options'
 
-// imports for DropdownMenu 
+// imports for DropdownMenu
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -16,8 +16,10 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from '@/components/ui/dropdown-menu'
 
+//imports for DataTableRowActions incorporated into this component, DataTableToolbar, on screens md and below
+import { LucideCopyPlus } from 'lucide-react'
 interface DataTableToolbarProps<TData> {
   table: Table<TData>
 }
@@ -30,63 +32,57 @@ export function DataTableToolbar<TData>({
   return (
     <div className="flex items-center justify-between">
       <Button size="sm" className="h-8 gap-1">
-                  <PlusCircle className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    New Task
-                  </span>
-                </Button>     
+        <PlusCircle className="h-3.5 w-3.5" />
+        <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+          New Task
+        </span>
+      </Button>
       <Search className="absolute left-2 top-2 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search Tasks..."
-              value={
-                (table.getColumn('taskDescription')?.getFilterValue() as string) ?? ''
-              }
-              onChange={(event) =>
-                table
-                  .getColumn('taskDescription')
-                  ?.setFilterValue(event.target.value)
-              }
-              className="w-[160px] h-8 rounded-lg bg-background pl-8 lg:w-[336px]"
-            /> 
-        {isFiltered && (
-          <Button
-            variant="ghost"
-            onClick={() => table.resetColumnFilters()}
-            className="h-8 px-2 lg:px-3"
-          >
-            <Cross2Icon className="ml-2 h-4 w-4" />
-          </Button>
-        )}
-        <div className="ml-auto flex items-center gap-2">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    {/* <Button variant="outline" size="sm" className="h-8 gap-1">
+      <Input
+        type="search"
+        placeholder="Search Tasks..."
+        value={
+          (table.getColumn('taskDescription')?.getFilterValue() as string) ?? ''
+        }
+        onChange={(event) =>
+          table.getColumn('taskDescription')?.setFilterValue(event.target.value)
+        }
+        className="w-[160px] h-8 rounded-lg bg-background pl-8 lg:w-[336px]"
+      />
+      {isFiltered && (
+        <Button
+          variant="ghost"
+          onClick={() => table.resetColumnFilters()}
+          className="h-8 px-2 lg:px-3"
+        >
+          <Cross2Icon className="ml-2 h-4 w-4" />
+        </Button>
+      )}
+      <div className="ml-auto flex items-center gap-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            {/* <Button variant="outline" size="sm" className="h-8 gap-1">
                       <ListFilter className="h-3.5 w-3.5" />
                       <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
                         Filter
                       </span>
                     </Button> */}
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuCheckboxItem checked>
-                      Active
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                      Archived
-                    </DropdownMenuCheckboxItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-                <Button size="sm" variant="outline" className="h-8 gap-1">
-                  <File className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Export
-                  </span>
-                </Button>
-              </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuCheckboxItem checked>Active</DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem>Archived</DropdownMenuCheckboxItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button size="sm" variant="outline" className="h-8 gap-1">
+          <LucideCopyPlus className="mr-2 h-4 w-4" />
+          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+            Duplicate
+          </span>
+        </Button>
+      </div>
       <DataTableViewOptions table={table} />
     </div>
   )
