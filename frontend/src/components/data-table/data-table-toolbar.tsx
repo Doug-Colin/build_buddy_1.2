6b129aa0150fad60d796130fac138a8b7383007a
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2024-04-20 08:10:00 +0200

    (feat) Add mobile-responsive Buttons & Icons to DataTableToolbar

--- frontend/src/components/data-table/data-table-toolbar.tsx
+++ frontend/src/components/data-table/data-table-toolbar.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react'
 import { Cross2Icon } from '@radix-ui/react-icons'
-import { Search }  from 'lucide-react'
+import { Search, File, PlusCircle }  from 'lucide-react'
 import { Table } from '@tanstack/react-table'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { DataTableViewOptions } from '@/components/data-table/data-table-view-options'
+
+// imports for DropdownMenu 
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 interface DataTableToolbarProps<TData> {
   table: Table<TData>
@@ -17,7 +28,13 @@ export function DataTableToolbar<TData>({
   const isFiltered = table.getState().columnFilters.length > 0
 
   return (
-    <div className="flex items-center justify-between">     
+    <div className="flex items-center justify-between">
+      <Button size="sm" className="h-8 gap-1">
+                  <PlusCircle className="h-3.5 w-3.5" />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                    New Task
+                  </span>
+                </Button>     
       <Search className="absolute left-2 top-2 h-4 w-4 text-muted-foreground" />
             <Input
               type="search"
@@ -41,6 +58,35 @@ export function DataTableToolbar<TData>({
             <Cross2Icon className="ml-2 h-4 w-4" />
           </Button>
         )}
+        <div className="ml-auto flex items-center gap-2">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    {/* <Button variant="outline" size="sm" className="h-8 gap-1">
+                      <ListFilter className="h-3.5 w-3.5" />
+                      <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                        Filter
+                      </span>
+                    </Button> */}
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuCheckboxItem checked>
+                      Active
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem>
+                      Archived
+                    </DropdownMenuCheckboxItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <Button size="sm" variant="outline" className="h-8 gap-1">
+                  <File className="h-3.5 w-3.5" />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                    Export
+                  </span>
+                </Button>
+              </div>
       <DataTableViewOptions table={table} />
     </div>
   )
