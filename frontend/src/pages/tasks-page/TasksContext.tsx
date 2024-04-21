d84eb5846aed57d5e0980db086d87a9b59e82bf1
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2024-04-21 07:45:20 +0200

    (feat: Tasks) Memoize value in TasksContext file to avoid rerendering on state change; add Redux slice CRUD+ functions to memoized value to make them available to all children of TasksContextProvider.

--- frontend/src/pages/tasks-page/TasksContext.tsx
+++ frontend/src/pages/tasks-page/TasksContext.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react'
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect, useMemo } from 'react'
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
+import {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+  duplicateTask,
+} from '@/features/tasks/taskSlice'
+//import { Task } from '@/types/types'
+
+// Contexts for each feature ensure that
 
 const TasksContext = createContext(null)
 
@@ -8,11 +18,35 @@ export const TasksProvider = ({ children }) => {
   const tasks = useAppSelector((state) => state.tasks.tasks)
   const dispatch = useAppDispatch()
 
+  // Fetch user tasks from Redux global state as soon as provider renders.
+  useEffect(() => {
+    dispatch(getTasks())
+  }, [dispatch])
+
+  const handleGetTasks = () => dispatch(getTasks())
+  const handleCreateTask = (taskId) => dispatch(createTask(taskId))
+  const handleUpdateTask = (taskId) => dispatch(updateTask(taskId))
+  const handleDeleteTask = (taskId) => dispatch(deleteTask(taskId))
+  const handleDuplicateTask = (taskId) => dispatch(duplicateTask(taskId))
+
+  const value = useMemo(
+    () => ({
+      tasks,
+      dispatch,
+      handleGetTasks,
+      handleCreateTask,
+      handleUpdateTask,
+      handleDeleteTask,
+      handleDuplicateTask,
+    }),
+    [tasks, dispatch],
+  )
+
   return (
-    <TasksContext.Provider value={{ tasks, dispatch }}>
-      {children}
-    </TasksContext.Provider>
+    <TasksContext.Provider value={{ value }}>{children}</TasksContext.Provider>
   )
 }
 
-export const useTasks = () => { useContext(TasksContext) }
+export const useTasks = () => {
+  useContext(TasksContext)
+}
